c45d6cbf98d69fdd885ee945c826c93fe5b3ce0c
pushock-name <pzabolotnyi@gmail.com>
2024-04-25 11:27:19 +0400

    fixes

--- script.js
+++ script.js
@@ -35,17 +35,6 @@ homeCards2.forEach(card => {
 
 //попапы
 
-
-let goose1Btn = document.querySelector('#goose-1__button')
-let goose1Popup = document.querySelector('#goose-1__popup')
-let goose1Close = document.querySelector('#goose-1__close__button')
-goose1Btn.addEventListener('click', () => {
-    goose1Popup.classList.toggle('home__goose__overlay--active')
-})
-goose1Close.addEventListener('click', () => {
-    goose1Popup.classList.remove('home__goose__overlay--active')
-})
-
 let rover1Btn = document.querySelector('#rover-1__button')
 let rover1Popup = document.querySelector('#rover-1__popup')
 let rover1Close = document.querySelector('#rover-1__close__button')
@@ -64,4 +53,27 @@ goose2Btn.addEventListener('click', () => {
 })
 goose2Close.addEventListener('click', () => {
     goose2Popup.classList.remove('wow__goose__overlay--active')
+})
+
+
+// let goose1Btn = document.querySelector('#goose-1__button')
+// let goose1Popup = document.querySelector('#goose-1__popup')
+// let goose1Close = document.querySelector('#goose-1__close__button')
+// goose1Btn.addEventListener('click', () => {
+//     goose1Popup.classList.toggle('home__goose__overlay--active')
+// })
+// goose1Close.addEventListener('click', () => {
+//     goose1Popup.classList.remove('home__goose__overlay--active')
+// })
+
+// хэдер
+
+let headerBurger = document.querySelectorAll('.mobile-header__button')
+let headerMenu = document.querySelectorAll('.mobile-header__menu')
+
+headerBurger.forEach(burger => {
+    burger.addEventListener('click', () => {
+        let menu = burger.parentNode.nextElementSibling
+        menu.classList.toggle('mobile-header__menu--active')
+    })
 })
